768a48574c02595654abd90401a1e0354a61b7b2
theander <aclaudino@gmail.com>
2022-07-11 22:43:58 -0300

    First commit

---
+++ src/main/java/com/healthmanagement/medicalapi/MedicalApiApplication.java
@@ -0,0 +1,13 @@
+package com.healthmanagement.medicalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalApiApplication.class, args);
+	}
+
+}

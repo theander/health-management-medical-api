21cd4696ac36f8f3f28f796506d3a7b82ed152c7
theander <aclaudino@gmail.com>
2022-08-12 23:16:24 -0300

    Renamed file and create environment variable to connect to database

--- src/main/java/com/healthmanagement/medicalapi/model/Medical.java
+++ src/main/java/com/healthmanagement/medicalapi/model/Medical.java
@@ -4,13 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
 public class Medical {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String evolution;
     private String recipe;
+    @ManyToOne
     private User user;
 }
--- src/main/java/com/healthmanagement/medicalapi/model/User.java
+++ src/main/java/com/healthmanagement/medicalapi/model/User.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GeneratorType;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -12,6 +13,7 @@ import javax.persistence.Id;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
